fb17b3aae462593868db3d1abbbd7ad70fe3e279
Jason <jason@zgwit.com>
2022-08-25 02:02:21 +0800

    添加标题栏

--- src/app/desktop/desktop.module.ts
+++ src/app/desktop/desktop.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { DesktopComponent } from './desktop.component';
 import {RouterModule, Routes} from "@angular/router";
 import {MatGridListModule} from "@angular/material/grid-list";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatRippleModule} from "@angular/material/core";
 
 const routes: Routes = [
   {
@@ -19,7 +23,11 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    MatGridListModule
+    MatGridListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatRippleModule
   ]
 })
 export class DesktopModule { }
